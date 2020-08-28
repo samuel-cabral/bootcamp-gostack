5ee1c0643abbf015a553af111fa91ae1ff8accac
Samuel Cabral <samuel61lima@gmail.com>
2020-08-28 20:35:59 -0300

    start: study States and Immutability

--- level01/frontend/src/App.js
+++ level01/frontend/src/App.js
@@ -1,32 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import Header from './components/Header';
 
 /**
- * Componente: 
- * Propriedade:
- *   uma informação que pode ser passada de um componente pai para um
- *   um componente filho.
- * Estado:
+ * Components: 
+ * Properties:
+ * State & Immutability:
 */
 
 
 function App() {
+  const [projects, setProjects] = useState(['To develop a new app', 'Front-end web']);
+
+  function handleAddProject() {
+    // projects.push(`New project ${Date.now()}`);
+
+    setProjects([...projects, `New project ${Date.now()}`]);  
+
+    console.log(projects);
+  }
+
   return (
     <>
-      <Header title="Homepage">
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-        </ul>
-      </Header>
+      <Header title="Projects" />
 
-      <Header title="Projects">
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-          <li>Login</li>
-        </ul>
-      </Header>
+      <ul>
+        {projects.map(project => <li key={project}>{project}</li>)}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>Add a new project</button>
     </>
   );
 }
